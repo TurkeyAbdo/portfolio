af241eb92c46868521a8bbbbd6b5cfe0719af1ac
TurkeyAbdo <abdulrhmanturkey123@gmail.com>
2025-06-24 12:00:46 +0200

    chnaged the ENV name to NEXT_PUBLIC_API_URL

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -118,7 +118,7 @@ export default function Home({ projects, services, testimonials, faqs }) {
 }
 
 export async function getServerSideProps() {
-  const apiUrl =  process.env.VITE_API_URL;
+  const apiUrl =  process.env.NEXT_PUBLIC_API_URL;
   
   try {
     const [projectsRes, servicesRes, testimonialsRes, faqsRes] = await Promise.all([
